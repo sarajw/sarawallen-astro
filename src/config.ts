797fb1651770742530976238c3ba5a8a6fdab503
SJW <88535714+sarajw@users.noreply.github.com>
2022-10-04 14:00:40 +0200

    slowly adding modals under work - only 1, needs fixing up

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Sara J. Wallén';
+export const SITE_TITLE = 'Sara Joy';
 export const SITE_DESCRIPTION = "Sara's homepage, blog, portfolio";
