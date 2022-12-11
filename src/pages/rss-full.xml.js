5ffa4ef45c9576e4757359fb498c956e6fe6ea76
SJW <88535714+sarajw@users.noreply.github.com>
2022-12-11 18:02:53 +0100

    fiddling with RSS

---
+++ src/pages/rss-full.xml.js
@@ -0,0 +1,19 @@
+import rss from '@astrojs/rss';
+import sanitizeHtml from 'sanitize-html';
+import { SITE_TITLE, SITE_DESCRIPTION } from '../config';
+
+const postImportResult = import.meta.glob('./blog/**/*.md', { eager: true }); 
+const posts = Object.values(postImportResult);
+
+export const get = () =>
+	rss({
+		title: SITE_TITLE,
+		description: SITE_DESCRIPTION,
+		site: import.meta.env.SITE,
+		items: posts.map((post) => ({
+			link: post.url,
+			title: post.frontmatter.title,
+			pubDate: post.frontmatter.pubDate,
+			content: sanitizeHtml(post.compiledContent()),
+		}))
+	});
