c17f991e3b2589d880241b4e62bc8eaf97901867
Sara Joy <88535714+sarajw@users.noreply.github.com>
2022-12-11 20:48:00 +0100

    Update rss-test.xml.js

--- src/pages/rss-test.xml.js
+++ src/pages/rss-test.xml.js
@@ -1,2 +1,19 @@
+import rss from '@astrojs/rss';
+import sanitizeHtml from 'sanitize-html';
+import { SITE_TITLE, SITE_DESCRIPTION } from '../config';
 
-
+const postImportResult = import.meta.glob('./blog/**/*.md', { eager: true }); 
+const posts = Object.values(postImportResult);
+
+export const get = () =>
+	rss({
+		title: SITE_TITLE+" | full content",
+		description: SITE_DESCRIPTION,
+		site: import.meta.env.SITE,
+		items: posts.map((post) => ({
+			link: post.url,
+			title: post.frontmatter.title,
+			pubDate: post.frontmatter.pubDate,
+			description: post.frontmatter.short ? sanitizeHtml(post.compiledContent()) : post.frontmatter.description, 
+		}))
+	});
