81bfeb2de94887a17a7aec0c9462bc45908b48fe
SJW <88535714+sarajw@users.noreply.github.com>
2022-12-11 18:09:12 +0100

    RSS content specified in feed title

--- src/pages/rss-full.xml.js
+++ src/pages/rss-full.xml.js
@@ -7,7 +7,7 @@ const posts = Object.values(postImportResult);
 
 export const get = () =>
 	rss({
-		title: SITE_TITLE,
+		title: SITE_TITLE+" | full content",
 		description: SITE_DESCRIPTION,
 		site: import.meta.env.SITE,
 		items: posts.map((post) => ({
--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -3,7 +3,7 @@ import { SITE_TITLE, SITE_DESCRIPTION } from '../config';
 
 export const get = () =>
 	rss({
-		title: SITE_TITLE,
+		title: SITE_TITLE+" | titles + descs only",
 		description: SITE_DESCRIPTION,
 		site: import.meta.env.SITE,
 		items: import.meta.glob('./blog/**/*.md'),
