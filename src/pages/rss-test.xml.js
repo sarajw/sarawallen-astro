d2dc8db7c42e8a52b0d5e6867b3d942508cf4c42
Sara Joy <88535714+sarajw@users.noreply.github.com>
2022-12-12 10:39:56 +0100

    coding without ide wheeee

--- src/pages/rss-test.xml.js
+++ src/pages/rss-test.xml.js
@@ -14,10 +14,6 @@ export const get = () =>
 			link: post.url,
 			title: post.frontmatter.title,
 			pubDate: post.frontmatter.pubDate,
-			if (post.frontmatter.short) {
-				content: sanitizeHtml(post.compiledContent()),
-			} else {
-				description: post.frontmatter.description,
-			}
+			post.frontmatter.short ? 'content: sanitizeHtml(post.compiledContent())' : 'description: post.frontmatter.description',
 		}))
 	});
