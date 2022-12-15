8ddd834e347552fe3eb64b8bf548fe184a00096d
SJW <88535714+sarajw@users.noreply.github.com>
2022-12-15 22:10:02 +0100

    rss fiddling and making links

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -3,7 +3,7 @@ import { SITE_TITLE, SITE_DESCRIPTION } from '../config';
 
 export const get = () =>
 	rss({
-		title: SITE_TITLE+" | titles + descs only",
+		title: SITE_TITLE,
 		description: SITE_DESCRIPTION,
 		site: import.meta.env.SITE,
 		items: import.meta.glob('./blog/**/*.md'),
