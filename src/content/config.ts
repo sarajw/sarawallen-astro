020f14e0f698fa49cc4286dbe6757a39e923a6b3
SJW <88535714+sarajw@users.noreply.github.com>
2023-04-08 22:00:41 +0200

    start collections migration

---
+++ src/content/config.ts
@@ -0,0 +1,26 @@
+// Import utilities from `astro:content`
+import { z, defineCollection } from "astro:content";
+// Define a schema for each collection you'd like to validate.
+const postsCollection = defineCollection({
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    pubDate: z.date(),
+    heroImage: z.string(),
+    caption: z.string()
+  })
+});
+const shortCollection = defineCollection({
+    schema: z.object({
+      title: z.string(),
+      short: z.boolean(),
+      pubDate: z.date(),
+      heroImage: z.string(),
+      caption: z.string()
+    })
+});
+// Export a single `collections` object to register your collection(s)
+export const collections = {
+  posts: postsCollection,
+  short: shortCollection,
+};
