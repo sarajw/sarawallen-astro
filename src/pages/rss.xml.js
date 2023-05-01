c18e6403c1ac32d960f8886259c1fae1747ba512
SJW <88535714+sarajw@users.noreply.github.com>
2023-05-01 16:26:33 +0200

    get normal rss feed back

---
+++ src/pages/rss.xml.js
@@ -0,0 +1,18 @@
+import rss from '@astrojs/rss';
+import getAllCollections from '../utils/getAllCollections.js';
+import { SITE_TITLE, SITE_DESCRIPTION } from '../config';
+
+export async function get() {
+  const posts = await getAllCollections();
+	return rss({
+		title: SITE_TITLE,
+		description: SITE_DESCRIPTION,
+		site: import.meta.env.SITE,
+		items: posts.map((post) => ({
+			title: post.data.title,
+			pubDate: post.data.pubDate,
+			description: post.data.description ?? "A short thort",
+			link: post.data.short ? "/blog/short/" + post.slug : "/blog/" + post.slug,
+		})),
+	});
+}
