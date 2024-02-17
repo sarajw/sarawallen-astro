d856d7336b326d3da8c981bd477bb141a3bcc74b
SJW <88535714+sarajw@users.noreply.github.com>
2024-02-17 23:37:25 +0100

    updated WIPs, added rssjoy pics - prep for stuff page, swap out HH photo

--- .astro/types.d.ts
+++ .astro/types.d.ts
@@ -452,6 +452,13 @@ declare module 'astro:content' {
   collection: "short";
   data: InferEntrySchema<"short">
 } & { render(): Render[".md"] };
+"2024-02-12-his-majesty-s-passport-office.md": {
+	id: "2024-02-12-his-majesty-s-passport-office.md";
+  slug: "2024-02-12-his-majesty-s-passport-office";
+  body: string;
+  collection: "short";
+  data: InferEntrySchema<"short">
+} & { render(): Render[".md"] };
 "allyship.md": {
 	id: "allyship.md";
   slug: "allyship";
