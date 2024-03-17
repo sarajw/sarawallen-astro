65d58f11922aac688ef745d8ce89236b636b1793
SJW <88535714+sarajw@users.noreply.github.com>
2024-03-17 13:52:26 +0100

    fix RSS feed

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -2,7 +2,7 @@ import rss from '@astrojs/rss';
 import getAllCollections from '../utils/getAllCollections.js';
 import { SITE_TITLE, SITE_DESCRIPTION } from '../config';
 
-export async function get() {
+export async function GET() {
   const posts = await getAllCollections();
 	return rss({
 		title: SITE_TITLE,
