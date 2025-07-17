499eb021d8ad40f610b55807daafae86ec077dd5
SJW <88535714+sarajw@users.noreply.github.com>
2025-07-17 23:46:01 +0200

    whimsica11y logo post and images

--- .astro/types.d.ts
+++ .astro/types.d.ts
@@ -141,84 +141,84 @@ declare module 'astro:content' {
   slug: "20230627";
   body: string;
   collection: "notes";
-  data: any
+  data: InferEntrySchema<"notes">
 } & { render(): Render[".md"] };
 "20230705.md": {
 	id: "20230705.md";
   slug: "20230705";
   body: string;
   collection: "notes";
-  data: any
+  data: InferEntrySchema<"notes">
 } & { render(): Render[".md"] };
 "20230717.md": {
 	id: "20230717.md";
   slug: "20230717";
   body: string;
   collection: "notes";
-  data: any
+  data: InferEntrySchema<"notes">
 } & { render(): Render[".md"] };
 "20230731.md": {
 	id: "20230731.md";
   slug: "20230731";
   body: string;
   collection: "notes";
-  data: any
+  data: InferEntrySchema<"notes">
 } & { render(): Render[".md"] };
 "20230822.md": {
 	id: "20230822.md";
   slug: "20230822";
   body: string;
   collection: "notes";
-  data: any
+  data: InferEntrySchema<"notes">
 } & { render(): Render[".md"] };
 "20231022.md": {
 	id: "20231022.md";
   slug: "20231022";
   body: string;
   collection: "notes";
-  data: any
+  data: InferEntrySchema<"notes">
 } & { render(): Render[".md"] };
 "20231212.md": {
 	id: "20231212.md";
   slug: "20231212";
   body: string;
   collection: "notes";
-  data: any
+  data: InferEntrySchema<"notes">
 } & { render(): Render[".md"] };
 "20231231.md": {
 	id: "20231231.md";
   slug: "20231231";
   body: string;
   collection: "notes";
-  data: any
+  data: InferEntrySchema<"notes">
 } & { render(): Render[".md"] };
 "20240130.md": {
 	id: "20240130.md";
   slug: "20240130";
   body: string;
   collection: "notes";
-  data: any
+  data: InferEntrySchema<"notes">
 } & { render(): Render[".md"] };
 "20240512.md": {
 	id: "20240512.md";
   slug: "20240512";
   body: string;
   collection: "notes";
-  data: any
+  data: InferEntrySchema<"notes">
 } & { render(): Render[".md"] };
 "20240714.md": {
 	id: "20240714.md";
   slug: "20240714";
   body: string;
   collection: "notes";
-  data: any
+  data: InferEntrySchema<"notes">
 } & { render(): Render[".md"] };
 "20240725.md": {
 	id: "20240725.md";
   slug: "20240725";
   body: string;
   collection: "notes";
-  data: any
+  data: InferEntrySchema<"notes">
 } & { render(): Render[".md"] };
 };
 "posts": {
@@ -227,119 +227,126 @@ declare module 'astro:content' {
   slug: "100days";
   body: string;
   collection: "posts";
-  data: any
+  data: InferEntrySchema<"posts">
 } & { render(): Render[".md"] };
 "a11y-aspects.md": {
 	id: "a11y-aspects.md";
   slug: "a11y-aspects";
   body: string;
   collection: "posts";
-  data: any
+  data: InferEntrySchema<"posts">
 } & { render(): Render[".md"] };
 "a11y-for-all.md": {
 	id: "a11y-for-all.md";
   slug: "a11y-for-all";
   body: string;
   collection: "posts";
-  data: any
+  data: InferEntrySchema<"posts">
 } & { render(): Render[".md"] };
 "color-scheme.md": {
 	id: "color-scheme.md";
   slug: "color-scheme";
   body: string;
   collection: "posts";
-  data: any
+  data: InferEntrySchema<"posts">
 } & { render(): Render[".md"] };
 "come-to-the-light-dark-side.md": {
 	id: "come-to-the-light-dark-side.md";
   slug: "come-to-the-light-dark-side";
   body: string;
   collection: "posts";
-  data: any
+  data: InferEntrySchema<"posts">
 } & { render(): Render[".md"] };
 "dance.md": {
 	id: "dance.md";
   slug: "dance";
   body: string;
   collection: "posts";
-  data: any
+  data: InferEntrySchema<"posts">
 } & { render(): Render[".md"] };
 "details.md": {
 	id: "details.md";
   slug: "details";
   body: string;
   collection: "posts";
-  data: any
+  data: InferEntrySchema<"posts">
 } & { render(): Render[".md"] };
 "job-application-tips.md": {
 	id: "job-application-tips.md";
   slug: "job-application-tips";
   body: string;
   collection: "posts";
-  data: any
+  data: InferEntrySchema<"posts">
 } & { render(): Render[".md"] };
 "my-church.md": {
 	id: "my-church.md";
   slug: "my-church";
   body: string;
   collection: "posts";
-  data: any
+  data: InferEntrySchema<"posts">
 } & { render(): Render[".md"] };
 "saras-side-step.md": {
 	id: "saras-side-step.md";
   slug: "saras-side-step";
   body: string;
   collection: "posts";
-  data: any
+  data: InferEntrySchema<"posts">
 } & { render(): Render[".md"] };
 "scrolling-tabs.md": {
 	id: "scrolling-tabs.md";
   slug: "scrolling-tabs";
   body: string;
   collection: "posts";
-  data: any
+  data: InferEntrySchema<"posts">
 } & { render(): Render[".md"] };
 "see-and-point.md": {
 	id: "see-and-point.md";
   slug: "see-and-point";
   body: string;
   collection: "posts";
-  data: any
+  data: InferEntrySchema<"posts">
 } & { render(): Render[".md"] };
 "the-web-changed.md": {
 	id: "the-web-changed.md";
   slug: "the-web-changed";
   body: string;
   collection: "posts";
-  data: any
+  data: InferEntrySchema<"posts">
 } & { render(): Render[".md"] };
 "uses.md": {
 	id: "uses.md";
   slug: "uses";
   body: string;
   collection: "posts";
-  data: any
+  data: InferEntrySchema<"posts">
 } & { render(): Render[".md"] };
 "watches.md": {
 	id: "watches.md";
   slug: "watches";
   body: string;
   collection: "posts";
-  data: any
+  data: InferEntrySchema<"posts">
 } & { render(): Render[".md"] };
 "what-screens-want.md": {
 	id: "what-screens-want.md";
   slug: "what-screens-want";
   body: string;
   collection: "posts";
-  data: any
+  data: InferEntrySchema<"posts">
+} & { render(): Render[".md"] };
+"whimsica11y-logo.md": {
+	id: "whimsica11y-logo.md";
+  slug: "whimsica11y-logo";
+  body: string;
+  collection: "posts";
+  data: InferEntrySchema<"posts">
 } & { render(): Render[".md"] };
 "write-websites.md": {
 	id: "write-websites.md";
   slug: "write-websites";
   body: string;
   collection: "posts";
-  data: any
+  data: InferEntrySchema<"posts">
 } & { render(): Render[".md"] };
 };
 "short": {
@@ -348,287 +355,287 @@ declare module 'astro:content' {
   slug: "2022-in-review";
   body: string;
   collection: "short";
-  data: any
+  data: InferEntrySchema<"short">
 } & { render(): Render[".md"] };
 "2023-01-06--html-css-js.md": {
 	id: "2023-01-06--html-css-js.md";
   slug: "2023-01-06--html-css-js";
   body: string;
   collection: "short";
-  data: any
+  data: InferEntrySchema<"short">
 } & { render(): Render[".md"] };
 "2023-01-08-bye-gmail-hi-fastmail.md": {
 	id: "2023-01-08-bye-gmail-hi-fastmail.md";
   slug: "2023-01-08-bye-gmail-hi-fastmail";
   body: string;
   collection: "short";
-  data: any
+  data: InferEntrySchema<"short">
 } & { render(): Render[".md"] };
 "2023-01-09-smartphone-woes.md": {
 	id: "2023-01-09-smartphone-woes.md";
   slug: "2023-01-09-smartphone-woes";
   body: string;
   collection: "short";
-  data: any
+  data: InferEntrySchema<"short">
 } & { render(): Render[".md"] };
 "2023-01-11-it-finally-got-me.md": {
 	id: "2023-01-11-it-finally-got-me.md";
   slug: "2023-01-11-it-finally-got-me";
   body: string;
   collection: "short";
-  data: any
+  data: InferEntrySchema<"short">
 } & { render(): Render[".md"] };
 "2023-01-18-ascii-art-heart.md": {
 	id: "2023-01-18-ascii-art-heart.md";
   slug: "2023-01-18-ascii-art-heart";
   body: string;
   collection: "short";
-  data: any
+  data: InferEntrySchema<"short">
 } & { render(): Render[".md"] };
 "2023-01-24-identity-parent.md": {
 	id: "2023-01-24-identity-parent.md";
   slug: "2023-01-24-identity-parent";
   body: string;
   collection: "short";
-  data: any
+  data: InferEntrySchema<"short">
 } & { render(): Render[".md"] };
 "2023-02-02-web-shedvelopment.md": {
 	id: "2023-02-02-web-shedvelopment.md";
   slug: "2023-02-02-web-shedvelopment";
   body: string;
   collection: "short";
-  data: any
+  data: InferEntrySchema<"short">
 } & { render(): Render[".md"] };
 "2023-02-14-slippery.md": {
 	id: "2023-02-14-slippery.md";
   slug: "2023-02-14-slippery";
   body: string;
   collection: "short";
-  data: any
+  data: InferEntrySchema<"short">
 } & { render(): Render[".md"] };
 "2023-02-27-international-rescue-forever.md": {
 	id: "2023-02-27-international-rescue-forever.md";
   slug: "2023-02-27-international-rescue-forever";
   body: string;
   collection: "short";
-  data: any
+  data: InferEntrySchema<"short">
 } & { render(): Render[".md"] };
 "2023-02-27-the-broom-of-trigger.md": {
 	id: "2023-02-27-the-broom-of-trigger.md";
   slug: "2023-02-27-the-broom-of-trigger";
   body: string;
   collection: "short";
-  data: any
+  data: InferEntrySchema<"short">
 } & { render(): Render[".md"] };
 "2023-06-28-pockets.md": {
 	id: "2023-06-28-pockets.md";
   slug: "2023-06-28-pockets";
   body: string;
   collection: "short";
-  data: any
+  data: InferEntrySchema<"short">
 } & { render(): Render[".md"] };
 "2023-07-20-mastodon-3.md": {
 	id: "2023-07-20-mastodon-3.md";
   slug: "2023-07-20-mastodon-3";
   body: string;
   collection: "short";
-  data: any
+  data: InferEntrySchema<"short">
 } & { render(): Render[".md"] };
 "2023-10-25-throbbers.md": {
 	id: "2023-10-25-throbbers.md";
   slug: "2023-10-25-throbbers";
   body: string;
   collection: "short";
-  data: any
+  data: InferEntrySchema<"short">
 } & { render(): Render[".md"] };
 "2023-12-12-m-pa-pa-m-pa-pa.md": {
 	id: "2023-12-12-m-pa-pa-m-pa-pa.md";
   slug: "2023-12-12-m-pa-pa-m-pa-pa";
   body: string;
   collection: "short";
-  data: any
+  data: InferEntrySchema<"short">
 } & { render(): Render[".md"] };
 "2023-12-30-by-webby-googley.md": {
 	id: "2023-12-30-by-webby-googley.md";
   slug: "2023-12-30-by-webby-googley";
   body: string;
   collection: "short";
-  data: any
+  data: InferEntrySchema<"short">
 } & { render(): Render[".md"] };
 "2024-01-27-bypassing-cors-using-netlify-redirects.md": {
 	id: "2024-01-27-bypassing-cors-using-netlify-redirects.md";
   slug: "2024-01-27-bypassing-cors-using-netlify-redirects";
   body: string;
   collection: "short";
-  data: any
+  data: InferEntrySchema<"short">
 } & { render(): Render[".md"] };
 "2024-02-12-his-majesty-s-passport-office.md": {
 	id: "2024-02-12-his-majesty-s-passport-office.md";
   slug: "2024-02-12-his-majesty-s-passport-office";
   body: string;
   collection: "short";
-  data: any
+  data: InferEntrySchema<"short">
 } & { render(): Render[".md"] };
 "2024-02-21-la-devise-du-web.md": {
 	id: "2024-02-21-la-devise-du-web.md";
   slug: "2024-02-21-la-devise-du-web";
   body: string;
   collection: "short";
-  data: any
+  data: InferEntrySchema<"short">
 } & { render(): Render[".md"] };
 "2024-03-18-people-blogs-guestbooks.md": {
 	id: "2024-03-18-people-blogs-guestbooks.md";
   slug: "2024-03-18-people-blogs-guestbooks";
   body: string;
   collection: "short";
-  data: any
+  data: InferEntrySchema<"short">
 } & { render(): Render[".md"] };
 "2024-04-07-tasty-water.md": {
 	id: "2024-04-07-tasty-water.md";
   slug: "2024-04-07-tasty-water";
   body: string;
   collection: "short";
-  data: any
+  data: InferEntrySchema<"short">
 } & { render(): Render[".md"] };
 "2024-05-18-good-box.md": {
 	id: "2024-05-18-good-box.md";
   slug: "2024-05-18-good-box";
   body: string;
   collection: "short";
-  data: any
+  data: InferEntrySchema<"short">
 } & { render(): Render[".md"] };
 "2024-06-30-it-s-just-fun.md": {
 	id: "2024-06-30-it-s-just-fun.md";
   slug: "2024-06-30-it-s-just-fun";
   body: string;
   collection: "short";
-  data: any
+  data: InferEntrySchema<"short">
 } & { render(): Render[".md"] };
 "2025-05-22-those-annoying-long-urls-causing-overflow.md": {
 	id: "2025-05-22-those-annoying-long-urls-causing-overflow.md";
   slug: "2025-05-22-those-annoying-long-urls-causing-overflow";
   body: string;
   collection: "short";
-  data: any
+  data: InferEntrySchema<"short">
 } & { render(): Render[".md"] };
 "allyship.md": {
 	id: "allyship.md";
   slug: "allyship";
   body: string;
   collection: "short";
-  data: any
+  data: InferEntrySchema<"short">
 } & { render(): Render[".md"] };
 "base-12-fingers.md": {
 	id: "base-12-fingers.md";
   slug: "base-12-fingers";
   body: string;
   collection: "short";
-  data: any
+  data: InferEntrySchema<"short">
 } & { render(): Render[".md"] };
 "coding-rollercoaster.md": {
 	id: "coding-rollercoaster.md";
   slug: "coding-rollercoaster";
   body: string;
   collection: "short";
-  data: any
+  data: InferEntrySchema<"short">
 } & { render(): Render[".md"] };
 "developing-developer.md": {
 	id: "developing-developer.md";
   slug: "developing-developer";
   body: string;
   collection: "short";
-  data: any
+  data: InferEntrySchema<"short">
 } & { render(): Render[".md"] };
 "dungarees.md": {
 	id: "dungarees.md";
   slug: "dungarees";
   body: string;
   collection: "short";
-  data: any
+  data: InferEntrySchema<"short">
 } & { render(): Render[".md"] };
 "i-am---.md": {
 	id: "i-am---.md";
   slug: "i-am---";
   body: string;
   collection: "short";
-  data: any
+  data: InferEntrySchema<"short">
 } & { render(): Render[".md"] };
 "i-dont-get-it.md": {
 	id: "i-dont-get-it.md";
   slug: "i-dont-get-it";
   body: string;
   collection: "short";
-  data: any
+  data: InferEntrySchema<"short">
 } & { render(): Render[".md"] };
 "i-gt-job.md": {
 	id: "i-gt-job.md";
   slug: "i-gt-job";
   body: string;
   collection: "short";
-  data: any
+  data: InferEntrySchema<"short">
 } & { render(): Render[".md"] };
 "just-build.md": {
 	id: "just-build.md";
   slug: "just-build";
   body: string;
   collection: "short";
-  data: any
+  data: InferEntrySchema<"short">
 } & { render(): Render[".md"] };
 "legitimate-interest.md": {
 	id: "legitimate-interest.md";
   slug: "legitimate-interest";
   body: string;
   collection: "short";
-  data: any
+  data: InferEntrySchema<"short">
 } & { render(): Render[".md"] };
 "little-lives-limerick.md": {
 	id: "little-lives-limerick.md";
   slug: "little-lives-limerick";
   body: string;
   collection: "short";
-  data: any
+  data: InferEntrySchema<"short">
 } & { render(): Render[".md"] };
 "meat-sacks.md": {
 	id: "meat-sacks.md";
   slug: "meat-sacks";
   body: string;
   collection: "short";
-  data: any
+  data: InferEntrySchema<"short">
 } & { render(): Render[".md"] };
 "reading-book-ux-ui.md": {
 	id: "reading-book-ux-ui.md";
   slug: "reading-book-ux-ui";
   body: string;
   collection: "short";
-  data: any
+  data: InferEntrySchema<"short">
 } & { render(): Render[".md"] };
 "refreshing.md": {
 	id: "refreshing.md";
   slug: "refreshing";
   body: string;
   collection: "short";
-  data: any
+  data: InferEntrySchema<"short">
 } & { render(): Render[".md"] };
 "tired-kids.md": {
 	id: "tired-kids.md";
   slug: "tired-kids";
   body: string;
   collection: "short";
-  data: any
+  data: InferEntrySchema<"short">
 } & { render(): Render[".md"] };
 "we-both-posted-our-luch.md": {
 	id: "we-both-posted-our-luch.md";
   slug: "we-both-posted-our-luch";
   body: string;
   collection: "short";
-  data: any
+  data: InferEntrySchema<"short">
 } & { render(): Render[".md"] };
 "what-ifs.md": {
 	id: "what-ifs.md";
   slug: "what-ifs";
   body: string;
   collection: "short";
-  data: any
+  data: InferEntrySchema<"short">
 } & { render(): Render[".md"] };
 };
 
@@ -640,5 +647,5 @@ declare module 'astro:content' {
 
 	type AnyEntryMap = ContentEntryMap & DataEntryMap;
 
-	export type ContentConfig = never;
+	export type ContentConfig = typeof import("./../src/content/config.js");
 }
---
+++ src/content/posts/whimsica11y-logo.md
@@ -0,0 +1,39 @@
+---
+title: "Work with Illustrators & Designers"
+description: "You might not have done it for a personal project before - but they're so much more fun to work with than AI."
+pubDate: "July 17 2025"
+heroImage: "/images/blog/whimsica11y.svg"
+caption: "In bubbly cartoonish style letters, 'whimsic' is written in uppercase in a curved rainbow shape. Underneath is the numeronym 'a11y'. The 'whimsic' curve has bands across it like a rainbow, using the colours from the diagonal of the Disability Pride flag."
+featured: featured
+mastoURL: ""
+---
+
+I'll admit it, some of the graphics on this site are AI-generated.
+
+You may have seen the very silly image at the top of the [Blogroll](/blog/roll/), or the watercolour images used for my [Weak Notes](/blog/notes) and [Short Thorts](/blog/short). All AI. I'm sorry. The rest are all more organically made.
+
+I would like to replace them, I used to be arty - I mean I could _totally_ make something like that myself - but I'm just so short on time that I never get around to it. And I'm so out of practice that I feel I'm just not going to be happy with anything I come up with.
+
+I decided the other day that what really needed a logo or header image (or something to double as both) was [whimsica11y.net](https://whimsica11y.net) - a side project that really isn't getting the love it deserves. And also, I want to have some stickers made. I like stickers.
+
+![A sketchbook full of pencil drawn attempts at a Whimsica11y logo. Some circular, some floral, one creepy, one a beach-ball, some trying to do fun typography, or cute cursive, and several smiley faces.](/images/blog/whimsica11y-sketches.jpg)
+
+And I do like drawing! So I drew a whole bunch of doodles while watching the kids. I wasn't finding a design direction that I liked, so I laid them down for a while. Until one day I picked the pencil up again and BOOM drew out something I really liked the look of!
+
+![Pencil drawing: In bubbly cartoonish style letters, 'whimsic' is written in uppercase in a curved rainbow shape. They have blocky edges, so they look three-dimensional. Underneath is the numeronym 'a11y', whose ending 'y' has its tail extended under the a11, forming an underline.](/images/blog/whimsica11y-final-idea.jpg)
+
+What made me really happy with this final design was two things - one was having a shape where I could incorporate a rainbow, and secondly as I extended the y underneath the rest of a11y it made me think of link underlines, and reminded me of [this slightly profane shirt](https://webbed-briefs.teemill.com/product/underline-your-links/) by the talented [Heydon](https://heydonworks.com).
+
+The challenge was then turning it into a clean, reusable, resizable graphic. I wanted it as an SVG. And I was already feeling [tired-as-an-emotion](https://www.youtube.com/watch?v=0sppw7Zq35w) even imagining doing that part.
+
+Well, eventually I remembered that I am in enough of a privileged position that I can pay someone to do this bit for me, and I happened to know someone who would really do it justice 💜
+
+With thanks to [Andy Carolan](https://www.andycarolan.com) for taking my drawing and SVG-ing it for me, and adding just a bit of his own touch, elevating it further! He was also very gracious about splitting it up into several layers, that I could go in to add and remove parts or change the colours easily myself.
+
+I'm still experimenting with the colours, and we'll see whether I'll also use the 3D version or not - but for now on [whimsica11y.net](https://whimsica11y.net) it's been given the [Disability Pride flag](https://www.goodhousekeeping.com/life/a43876914/disability-pride-flag/) treatment :)
+
+![Screenshot of whimsica11y.net with the new logo prominently in the centre of the lannding page](/images/blog/whimsica11y-screenshot.jpg)
+
+So instead of "prompt engineering", and trying to finagle an AI into producing the image you want from its scrapings of the entire internet - consider drawing something! Then find yourself a friendly illustrator.
+
+It doesn't matter if your drawing is scrappy, these people can create wonders from simple ideas. You can then work with them to build something that's still very much yours as much as it is theirs. There's so much joy in having your own work returned to you, made way better!
